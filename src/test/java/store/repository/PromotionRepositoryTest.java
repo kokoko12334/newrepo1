17e7cd4de3cbee7ba20c4814a77cd7220635077f
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-11-08 20:03:11 +0900

    refactor:  간격 수정

--- src/test/java/store/repository/PromotionRepositoryTest.java
+++ src/test/java/store/repository/PromotionRepositoryTest.java
@@ -1,18 +1,17 @@
 package store.repository;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import store.dto.PromotionDTO;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class PromotionRepositoryTest {
 
     @Test
     void loadTest() {
         PromotionRepository repository = new PromotionRepository();
-
         String name = "탄산2+1";
+
         PromotionDTO promotion = repository.findById(name);
 
         assertThat(promotion).isInstanceOf(PromotionDTO.class);
