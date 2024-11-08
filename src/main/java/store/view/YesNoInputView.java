194d695e3c949aac8820d0948a6d8b0bc888803d
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-11-09 00:40:15 +0900

    feat: YesNo 인풋 검증 구현

--- src/main/java/store/view/YesNoInputView.java
+++ src/main/java/store/view/YesNoInputView.java
@@ -1,8 +1,12 @@
 package store.view;
 
+import store.error.ErrorMessages;
+
 public class YesNoInputView extends InputView {
     @Override
     public void validate(String input) {
-
+        if (!input.equals("Y") && !input.equals("N")) {
+            throw new IllegalArgumentException(ErrorMessages.NOT_VALID_FORMAT.getMessage());
+        }
     }
 }
