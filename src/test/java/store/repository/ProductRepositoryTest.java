eb9c65a1722c2a06859007780df7c6fcb8b13b45
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-11-08 20:27:03 +0900

    refactor: promtions, products 네이밍 수정

--- src/test/java/store/repository/ProductRepositoryTest.java
+++ src/test/java/store/repository/ProductRepositoryTest.java
@@ -10,10 +10,11 @@ public class ProductRepositoryTest {
 
     @Test
     void loadTest() {
-        ProductRepository repository = new ProductRepository();
+        PromotionRepository promotions = new PromotionRepository();
+        ProductRepository products = new ProductRepository(promotions);
         String key = "콜라:탄산2+1";
 
-        ProductDTO product = repository.findById(key);
+        ProductDTO product = products.findById(key);
 
         assertThat(key).isEqualTo(product.getCompositeKey());
         assertThat("콜라").isEqualTo(product.getName());
--- src/test/java/store/repository/PromotionRepositoryTest.java
+++ src/test/java/store/repository/PromotionRepositoryTest.java
@@ -9,10 +9,10 @@ public class PromotionRepositoryTest {
 
     @Test
     void loadTest() {
-        PromotionRepository repository = new PromotionRepository();
+        PromotionRepository promotions = new PromotionRepository();
         String name = "탄산2+1";
 
-        PromotionDTO promotion = repository.findById(name);
+        PromotionDTO promotion = promotions.findById(name);
 
         assertThat(promotion).isInstanceOf(PromotionDTO.class);
         assertThat(name).isEqualTo(promotion.getName());
