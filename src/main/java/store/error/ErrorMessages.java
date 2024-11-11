76205d2c9fc16bf40b3c904bf412321c2b9924c0
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-11-11 23:33:57 +0900

    feat: 에러메시지 추가

--- src/main/java/store/error/ErrorMessages.java
+++ src/main/java/store/error/ErrorMessages.java
@@ -3,7 +3,8 @@ package store.error;
 public enum ErrorMessages {
     NOT_VALID_FORMAT("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요."),
     PRODUCT_NOT_FOUND("[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요."),
-    EXCEEDS_STOCK_QUANTITY("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
+    EXCEEDS_STOCK_QUANTITY("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요."),
+    OUT_OF_STOCK("[ERROR] 재고 수량이 없습니다. 다시 입력해 주세요.");
 
     private final String message;
 
