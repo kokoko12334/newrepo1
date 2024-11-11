e0fd5efc39eb390c45b3caedeb9d130ef5455eb1
KOMINPYO <gnjgnjgnj1@gmail.com>
2024-11-11 23:33:49 +0900

    feat: 메인함수 설계

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,21 @@
 package store;
 
+import camp.nextstep.edu.missionutils.DateTimes;
+import store.controller.StoreController;
+import store.service.ProductService;
+import store.service.SetupService;
+import store.view.InputView;
+import store.view.OrderInputView;
+
+import java.time.LocalDateTime;
+
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ProductService productService = SetupService.load();
+        InputView orderInputView = new OrderInputView(productService);
+        StoreController controller = new StoreController(productService, orderInputView);
+        LocalDateTime currentDate = DateTimes.now();
+        controller.run(currentDate);
     }
 }
